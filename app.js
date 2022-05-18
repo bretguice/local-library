cb62d0fa9fd7648c31a4784ce3e8b2783cfa1b7c
bretguice <89871624+bretguice@users.noreply.github.com>
2022-05-18 16:30:36 -0500

    added changes for production on heroku

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog')
 
+var compression = require('compression')
+var helmet = require('helmet')
+
 var app = express();
 
+app.use(helmet());
+
 var mongoose = require('mongoose')
-var mongoDB = 'mongodb+srv://library:locallibrary@library.nhsyh.mongodb.net/local_library?retryWrites=true&w=majority'
+
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://library:locallibrary@library.nhsyh.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -25,6 +33,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
